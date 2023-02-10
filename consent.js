096b6bf638ef036959f6319f6cca5a5dd2f6bee2
kazhashimoto <zkar.h62@gmail.com>
2023-02-11 01:48:52 +0900

    Add consent.js

---
+++ consent.js
@@ -0,0 +1,23 @@
+// https://developers.google.com/tag-platform/tag-manager/templates/consent-apis?hl=ja
+
+// Array of callbacks to be executed when consent changes
+const consentListeners = [];
+
+/**
+ * Called from GTM template to set callback to be executed when user consent is provided.
+ * @param {function} Callback to execute on user consent
+ */
+window.addConsentListenerExample = (callback) => {
+  console.log('addConsentListenerExample', callback);
+  consentListeners.push(callback);
+};
+
+/**
+ * Called when user grants/denies consent.
+ * @param {Object} Object containing user consent settings.
+ */
+const onConsentChange = (consent) => {
+  consentListeners.forEach((callback) => {
+    callback(consent);
+  });
+};
