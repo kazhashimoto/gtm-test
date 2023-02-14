6ae55ac9849c2fb057ed85a41ebf709215fef57c
kazhashimoto <zkar.h62@gmail.com>
2023-02-14 22:58:54 +0900

    Add checkboxes of consent type to banner

--- banner.js
+++ banner.js
@@ -1,19 +1,39 @@
+const domString = `
+<div class="banner">
+  <div class="banner-inner">
+    <dl class="col1">
+      <dt>Consent Types: </dt>
+      <dd>
+        <label for="cb1"><input type="checkbox" id="cb1" value="adConsentGranted" class="cb">ad</label>
+        <label for="cb2"><input type="checkbox" id="cb2" value="analyticsConsentGranted" class="cb">analytics</label>
+        <label for="cb3"><input type="checkbox" id="cb3" value="functionalityConsentGranted" class="cb">functionality</label>
+        <label for="cb4"><input type="checkbox" id="cb4" value="personalizationConsentGranted" class="cb">personalization</label>
+        <label for="cb5"><input type="checkbox" id="cb5" value="securityConsentGranted" class="cb">security</label>
+      </dd>
+    </dl>
+    <div class="col2">
+      <button type="button" id="ok-btn">OK</button>
+    </div>
+  </div>
+</div>
+`;
+
 (function() {
   console.log('### banner.js');
-  const target = document.createElement('div');
-  target.classList.add('banner');
+  let banner = document.createRange().createContextualFragment(domString);
+  banner.querySelector('#ok-btn').addEventListener('click', (evt) => {
+    console.log('click', evt.target);
+    const consent = {};
+    const items = document.querySelectorAll('.cb');
+    for (const el of items) {
+      console.log(el.id, el.checked, el.value);
+      if (el.checked) {
+        consent[el.value] = true;
+      }
+    }
+    console.log(consent);
+    onConsentChange(consent);
+  });
 
-  let el = (function() {
-    const el = document.createElement('button');
-    el.type = 'button';
-    el.id = 'banner-btn';
-    el.textContent = 'OK';
-    el.addEventListener('click', function(evt) {
-      console.log('clicked', evt.target);
-    });
-    return el;
-  })();
-
-  target.appendChild(el);
-  document.body.appendChild(target);
+  document.body.appendChild(banner);
 })();
